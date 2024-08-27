7c5ecfa0a15132b77e1d59ce504cfd7dfba3dfe0
henry <hernyrizki909@gmail.com>
2024-08-27 14:11:08 +0700

    Portofolio Henry

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            document.querySelector(".navbar").classList.add("scrolled");
+        } else {
+            document.querySelector(".navbar").classList.remove("scrolled");
+        }
+    });
+});
